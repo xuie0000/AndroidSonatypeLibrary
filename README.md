e130033ae692264b228aa66e04f5c4a00082d47a
Jie Xu <xujie@anzer.com.cn>
2021-03-10 11:03:05 +0800

    usage + README.md

---
+++ README.md
@@ -0,0 +1,17 @@
+# Sonatype Maven Central Sample
+
+## Result
+available on `mavenCentral()`
+
+```
+implementation 'com.xuie0000:util-tools:1.2.2'
+```
+
+## Sample
+
+see code `:utils`
+
+## Reference
+- https://juejin.cn/post/6932485276124233735
+- https://my.oschina.net/ramostear/blog/4332991
+- https://www.jianshu.com/p/67d81977b027
--- app/src/main/java/com/xuie0000/sonatype/MainActivity.kt
+++ app/src/main/java/com/xuie0000/sonatype/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.xuie0000.sonatype
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.xuie0000.VersionHelper
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    // test library
+    Log.d("MainActivity", "version:${VersionHelper.getVersionName(this)}")
   }
 }
